7fbed4f143502375c40ba94a9ef0a19797a60e7f
fangwt <fangwt@sucsoft.com>
2022-12-02 08:47:38 +0800

    fix: ‘副本’

---
+++ positionList.js
@@ -0,0 +1,102 @@
+const positionList = [
+    {
+        latitude: 30.26615,
+        longitude: 120.12042,
+    },
+    {
+        latitude: 30.2663,
+        longitude: 120.12085,
+    },
+    {
+        latitude: 30.26627,
+        longitude: 120.12115,
+    },
+    {
+        latitude: 30.26621,
+        longitude: 120.12146,
+    },
+    {
+        latitude: 30.26664,
+        longitude: 120.12057,
+    },
+    {
+        latitude: 30.26661,
+        longitude: 120.12111,
+    },
+    {
+        latitude: 30.26652,
+        longitude: 120.12167,
+    },
+    {
+        latitude: 30.26598,
+        longitude: 120.1221,
+    },
+    {
+        latitude: 30.26575,
+        longitude: 120.12223,
+    },
+    {
+        latitude: 30.26532,
+        longitude: 120.12178,
+    },
+    {
+        latitude: 30.26489,
+        longitude: 120.12149,
+    },
+    {
+        latitude: 30.26607,
+        longitude: 120.12168,
+    },
+    {
+        latitude: 30.2667,
+        longitude: 120.11995,
+    },
+    {
+        latitude: 30.26635,
+        longitude: 120.12259,
+    },
+    {
+        latitude: 30.26603,
+        longitude: 120.1228,
+    },
+    {
+        latitude: 30.26716,
+        longitude: 120.12088,
+    },
+    {
+        latitude: 30.26725,
+        longitude: 120.12035,
+    },
+    {
+        latitude: 30.26697,
+        longitude: 120.12159,
+    },
+    {
+        latitude: 30.26684,
+        longitude: 120.12215,
+    },
+];
+
+const o = [
+    [9, 3],
+    [1, 3],
+    [1, 3],
+    [2, 3],
+    [2, 4],
+    [5, 6],
+    [2, 6],
+    [3, 7],
+    [10, 3],
+    [5, 10],
+    [3, 15],
+    [10, 16],
+    [11, 18],
+    [9, 19],
+    [5, 15],
+    [6, 10],
+    [6, 12],
+    [1, 10],
+    [2, 14],
+    [3, 9],
+    [4, 10],
+];
